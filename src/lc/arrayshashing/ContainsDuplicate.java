f407b0b4093ee5ede5d4a42209fb7f6a0cc7b947
stian <stsla0672@oslomet.no>
2025-10-22 12:00:41 +0200

    La til noen kommentarer

--- src/lc/arrayshashing/ContainsDuplicate.java
+++ src/lc/arrayshashing/ContainsDuplicate.java
@@ -4,22 +4,6 @@ import java.util.HashSet;
 
 public class ContainsDuplicate {
 
-    /*
-     * public boolean hasDuplicate(int[] nums) {
-     * 
-     * for (int i = 0; i < nums.length; i++) { // bruk -1 dersom du må se framover
-     * (i+1) i arrayet, ikke nødvendig her
-     * // pga 2 løkker.
-     * for (int j = i + 1; j < nums.length; j++) {
-     * if (nums[i] == nums[j]) {
-     * return true;
-     * }
-     * }
-     * }
-     * return false;
-     * }
-     */
-
     public boolean hasDuplicate(int[] nums) {
         HashSet<Integer> numSet = new HashSet<>(); // lagret et tomt HashSet som lagrer tall vi har sett
         for (int num : nums) { // iterer over arrayet nums
@@ -36,4 +20,21 @@ public class ContainsDuplicate {
         boolean result = cd.hasDuplicate(new int[] { 1, 2, 3, 4 });
         System.out.println(result);
     }
+
+    // Brute force løsning
+    /*
+     * public boolean hasDuplicate(int[] nums) {
+     * 
+     * for (int i = 0; i < nums.length; i++) { // bruk -1 dersom du må se framover
+     * (i+1) i arrayet, ikke nødvendig her
+     * // pga 2 løkker.
+     * for (int j = i + 1; j < nums.length; j++) {
+     * if (nums[i] == nums[j]) {
+     * return true;
+     * }
+     * }
+     * }
+     * return false;
+     * }
+     */
 }
